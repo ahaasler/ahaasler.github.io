b6f2087130a815f6a6a567194d9da20473fe4a24
Adrian Haasler García <dev@adrianhaasler.com>
2020-05-24 20:37:59 +0200

    Move strings to config json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
-export default function Index() {
+import { GetStaticProps } from 'next'
+
+export default function Index({
+	page
+}: {
+	page: {
+		title: string
+		description: string
+	}
+}) {
 	return (
 		<>
-			<h1>Adrian Haasler</h1>
-			<h2>My Next.js web</h2>
+			<h1>{page.title}</h1>
+			<h2>{page.description}</h2>
 		</>
 	)
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	return {
+		props: {
+			page: (await import('../config/page.json')).default
+		}
+	}
 }
