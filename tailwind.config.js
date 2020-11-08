a63ade63f79cbaa6de0588c9e1fdd2f51469cd79
Adrian Haasler García <dev@adrianhaasler.com>
2020-11-08 22:55:56 +0100

    Purge unused CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
 	plugins: [
 		require('@tailwindcss/custom-forms')
-	]
+	],
+	purge: {
+		enabled: process.env.NODE_ENV === 'production',
+		content: [
+			"./pages/**/*.tsx"
+		]
+	}
 }
