c4a9187572363228b7dc073161b69734805bacc7
Adrian Haasler García <dev@adrianhaasler.com>
2020-11-11 23:52:03 +0100

    Use tailwind colors in dark and light themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
 	plugins: [
-		require('@tailwindcss/custom-forms')
+		require("@tailwindcss/custom-forms")
 	],
 	purge: {
-		enabled: process.env.NODE_ENV === 'production',
+		enabled: process.env.NODE_ENV === "production",
 		content: [
 			"./pages/**/*.tsx"
 		]
+	},
+	theme: {
+		extend: {
+			colors: {
+				bg: {
+					primary: "var(--bg-bg-primary)"
+				},
+				text: {
+					primary: "var(--text-text-primary)",
+					secondary: "var(--text-text-secondary)"
+				}
+			}
+		}
 	}
 }
